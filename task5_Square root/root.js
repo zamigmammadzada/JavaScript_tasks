c0cac9b9e370527a2fa100547537f632794037d8
Zamig Mammadzada <memmedzadezamiq@gmail.com>
2022-07-21 23:04:49 +0400

    Add files via upload

---
+++ task5_Square root/root.js
@@ -0,0 +1,4 @@
+let number = prompt ('Ədədi daxil edin:');
+let result = Math.sqrt(number);
+document.write (result);
+//kechen defe result'u alert ile usere gostermishdim, bu defe sehifenin icinde yazmagi denedim
