95a612e913d48bcf870b224b048ebe64918508a8
Zamig Mammadzada <memmedzadezamiq@gmail.com>
2022-07-24 13:47:23 +0400

    Add files via upload

---
+++ task7_PlusOrMinus/plusminus.js
@@ -0,0 +1,10 @@
+let userNumber = prompt('Ədədi daxil edin')
+if(userNumber > 0){
+  console.log(' Ədəd müsbətdir')
+}
+else if(userNumber < 0){
+  console.log('Ədəd mənfidir')
+}
+else{
+  console.log('Ədəd sıfırdır')
+}
