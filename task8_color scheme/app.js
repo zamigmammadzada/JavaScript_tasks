f1b36465de24b86132820fea3b22ee8ff9e53112
Zamig Mammadzada <memmedzadezamiq@gmail.com>
2022-08-22 23:44:13 +0400

    Add files via upload

---
+++ task8_color scheme/app.js
@@ -0,0 +1,12 @@
+function grayColor(){
+    document.body.style.backgroundColor = "gray"
+}
+function redColor(){
+    document.body.style.backgroundColor = "red"
+}
+function blueColor(){
+    document.body.style.backgroundColor = "blue"
+}
+function yellowColor(){
+    document.body.style.backgroundColor = 'yellow'
+}
